4849d00f0a09d675591f1e43d36e06a86257d8ff
feeikz <zajacfrederik97@gmail.com>
2020-07-18 20:03:24 +0200

    Final version
    CRUD operations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
+  
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { environment } from '../environments/environment.prod';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
 import { AppComponent } from './app.component';
+import { ItemsComponent } from './components/items/items.component';
+
+import { ItemService } from './services/item.service';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AddItemComponent } from './components/add-item/add-item.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent,
+    NavbarComponent,
+    AddItemComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase, 'angularfs'),
+    AngularFirestoreModule
   ],
-  providers: [],
+  providers: [ItemService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB3WdYaW-CPxCEp0LF3vAC_pV9lrrjk_GQ",
+    authDomain: "fs1prod-9a97c.firebaseapp.com",
+    databaseURL: "https://fs1prod-9a97c.firebaseio.com",
+    projectId: "fs1prod-9a97c",
+    storageBucket: "fs1prod-9a97c.appspot.com",
+    messagingSenderId: "751841820568",
+    appId: "1:751841820568:web:4ed802b4e62cae9964b5ed",
+    measurementId: "G-WY281HX6P5"
+  }
 };
